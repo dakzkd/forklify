09ab0e85b0e863f3655b3d123eaf1b3d04c8997e
dakzkd <damir.kadyr@gmail.com>
2021-07-24 21:59:05 +0600

    final

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,5 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'eb8f17d7-5233-4218-ab3c-d68ebdb85a25';
+export const MODAL_CLOSE_SEC = 2.5;
